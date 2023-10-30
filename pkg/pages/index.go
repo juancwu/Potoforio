678e2fc0d52214565cbda02b2e0ba89a27f9316e
jc <46619361+juancwu@users.noreply.github.com>
2023-10-29 22:56:41 -0400

    updates to landing page

--- pkg/pages/index.go
+++ pkg/pages/index.go
@@ -1,95 +1,95 @@
 package pages
 
 import (
-    "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
 
 type Tech struct {
-    TechName string
-    TechURL string
-    TechClasses string
+	TechName    string
+	TechURL     string
+	TechClasses string
 }
 
 type Project struct {
-    Name string
-    URL string
-    Description string
-    TechList []Tech
-    Repo string
-    Classes string
+	Name        string
+	URL         string
+	Description string
+	TechList    []Tech
+	Repo        string
+	Classes     string
 }
 
 type Page struct {
-    Projects []Project
+	Projects []Project
 }
 
 func Index(c echo.Context) error {
 
-    data := Page{}
-    data.Projects = []Project{
-        {
-            Name: "Shoto",
-            URL: "https://www.shoto.at",
-            Description: "Simple and effective URL shortoner.",
-            Repo: "https://github.com/juancwu/shoto",
-            Classes: "border-indigo-500 hover:bg-indigo-950 hover:shadow-indigo-500/50",
-            TechList: []Tech{
-                {
-                    TechName: "Next.js",
-                    TechURL: "https://nextjs.org/",
-                    TechClasses: "bg-zinc-950 text-zinc-100",
-                },
-                {
-                    TechName: "Drizzle ORM",
-                    TechURL: "https://orm.drizzle.team/",
-                    TechClasses: "bg-zinc-950 text-zinc-100",
-                },
-                {
-                    TechName: "Turso",
-                    TechURL: "https://turso.tech/",
-                    TechClasses: "bg-zinc-950 text-zinc-100",
-                },
-            },
-        },
-        {
-            Name: "Bento",
-            URL: "",
-            Description: "API service to manage projects' secrets or environment variables with the ability to separate the variables into their own environment.",
-            Repo: "https://github.com/juancwu/bento",
-            Classes: "border-sky-500 hover:bg-sky-950 hover:shadow-sky-500/50",
-            TechList: []Tech{
-                {
-                    TechName: "Golang",
-                    TechURL: "https://go.dev/",
-                    TechClasses: "bg-zinc-950 text-zinc-100",
-                },
-                {
-                    TechName: "Chi",
-                    TechURL: "https://go-chi.io/#/",
-                    TechClasses: "bg-zinc-950 text-zinc-100",
-                },
-                {
-                    TechName: "PlanetScale",
-                    TechURL: "https://planetscale.com/",
-                    TechClasses: "bg-zinc-950 text-zinc-100",
-                },
-            },
-        },
-        {
-            Name: "Interpreter In Go",
-            URL: "",
-            Description: "Custom programming language interpreter written with go.",
-            Repo: "https://github.com/juancwu/interpretor-in-go",
-            Classes: "border-pink-500 hover:bg-pink-950 hover:shadow-pink-500/50",
-            TechList: []Tech{
-                {
-                    TechName: "Golang",
-                    TechURL: "https://go.dev/",
-                    TechClasses: "bg-zinc-950 text-zinc-100",
-                },
-            },
-        },
-    }
+	data := Page{}
+	data.Projects = []Project{
+		{
+			Name:        "Shoto",
+			URL:         "https://www.shoto.at",
+			Description: "Simple and effective URL shortoner.",
+			Repo:        "https://github.com/juancwu/shoto",
+			Classes:     "border-indigo-500",
+			TechList: []Tech{
+				{
+					TechName:    "Next.js",
+					TechURL:     "https://nextjs.org/",
+					TechClasses: "transition bg-zinc-950 text-zinc-100 hover:bg-zinc-100 hover:text-gray-950",
+				},
+				{
+					TechName:    "Drizzle ORM",
+					TechURL:     "https://orm.drizzle.team/",
+                    TechClasses: "transition bg-zinc-950 text-zinc-100 hover:bg-green-600",
+				},
+				{
+					TechName:    "Turso",
+					TechURL:     "https://turso.tech/",
+                    TechClasses: "transition bg-zinc-950 text-zinc-100 hover:bg-teal-600",
+				},
+			},
+		},
+		{
+			Name:        "Bento",
+			URL:         "",
+			Description: "API service to manage projects' secrets or environment variables with the ability to separate the variables into their own environment.",
+			Repo:        "https://github.com/juancwu/bento",
+			Classes:     "border-sky-500",
+			TechList: []Tech{
+				{
+					TechName:    "Golang",
+					TechURL:     "https://go.dev/",
+					TechClasses: "transition bg-zinc-950 text-zinc-100",
+				},
+				{
+					TechName:    "Chi",
+					TechURL:     "https://go-chi.io/#/",
+					TechClasses: "bg-zinc-950 text-zinc-100",
+				},
+				{
+					TechName:    "PlanetScale",
+					TechURL:     "https://planetscale.com/",
+					TechClasses: "bg-zinc-950 text-zinc-100",
+				},
+			},
+		},
+		{
+			Name:        "Interpreter In Go",
+			URL:         "",
+			Description: "Custom programming language interpreter written with Go following the book \"Writing an Interpreter In Go\" By Thorsten Ball",
+			Repo:        "https://github.com/juancwu/interpretor-in-go",
+			Classes:     "border-emerald-500",
+			TechList: []Tech{
+				{
+					TechName:    "Golang",
+					TechURL:     "https://go.dev/",
+					TechClasses: "bg-zinc-950 text-zinc-100",
+				},
+			},
+		},
+	}
 
-    return c.Render(200, "index.html", data)
+	return c.Render(200, "index.html", data)
 }
