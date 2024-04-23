7f91210821778c28cb5ed2526b78af044065659c
jc <46619361+juancwu@users.noreply.github.com>
2024-04-22 23:08:40 -0400

    graceful shutdown

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -18,6 +23,12 @@ type TemplateRenderer struct {
 }
 
 func main() {
+	// create a channel to listen for signals
+	sigChan := make(chan os.Signal, 1)
+
+	// listen to SIGTERM and SIGINT (ctrl-c)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+
 	// Load connection string from .env file
 	if os.Getenv("GO_ENV") != "production" {
 		fmt.Println("Loading env...")
@@ -33,28 +44,59 @@ func main() {
 		os.Exit(1)
 	}
 
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v\n", err)
+	}
+	defer func() {
+		// shutdown database connection
+		if err := conn.Close(context.Background()); err != nil {
+			log.Printf("Error closing database connection: %v\n", err)
+		} else {
+			log.Println("Database connection closed gracefully.")
+		}
+	}()
+
 	e := echo.New()
 	e.Renderer = &TemplateRenderer{
 		templates: templates,
 	}
 
-	e.Use(middleware.Logger())
-	e.Static("/static", "static")
+	// serve server in a goroutine, allow the code to listen to ctrl-c
+	go func() {
+		e.Use(middleware.Logger())
+		e.Static("/static", "static")
 
-	e.GET("/", renderPage)
+		e.GET("/", renderPage)
 
-	e.GET("/service/health-check", func(c echo.Context) error {
-		c.Response().Writer.WriteHeader(http.StatusOK)
-		c.Response().Write([]byte("APP VERSION: 1.0.0"))
-		return nil
-	})
+		e.GET("/service/health-check", func(c echo.Context) error {
+			c.Response().Writer.WriteHeader(http.StatusOK)
+			c.Response().Write([]byte("APP VERSION: 1.0.0"))
+			return nil
+		})
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "5173"
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "5173"
+		}
+
+		e.Start(fmt.Sprintf(":%s", port))
+	}()
+
+	// block until a signal is received
+	sig := <-sigChan
+	log.Printf("Caught signal: %s\n", sig)
+
+	// start graceful shutdown with a timeout
+	// should stop everything and clean up within 10 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		log.Printf("Error during server shutdown: %v\n", err)
+	} else {
+		log.Println("Server shut down gracefully.")
 	}
-
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
 }
 
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
